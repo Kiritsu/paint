db5ccc16ee74ceb8a6091c1a5ee28113e4dfd925
Allan <allan.mercou@icloud.com>
2019-03-20 09:35:47 +0100

    Adds documentation to our classes.

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,10 @@
 import java.net.Socket;
 
+/**
+ * This class represents a Client that will connect to a server and send different packets depending on the client's action.
+ * @author Allan Mercou, Mathieu Lagnel, Gabriel Cousin
+ * @version 1.0
+ */
 public class Client {
     private Server server;
     private Socket clientSocket;
--- src/ClientAccepterService.java
+++ src/ClientAccepterService.java
@@ -1,2 +1,7 @@
+/**
+ * This class will be used as a server that will handle connection of the clients.
+ * @author Allan Mercou, Mathieu Lagnel, Gabriel Cousin
+ * @version 1.0
+ */
 public class ClientAccepterService {
 }

2f90f06f3926aa9455f34aa2eaf1724b9fa13385
Allan <allan.mercou@icloud.com>
2019-03-20 10:24:10 +0100

    Adds a simple logger.

--- src/Logger.java
+++ src/Logger.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Logger {
+    public synchronized static void println(LogLevel level, String source, String message) {
+        String dateTime = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
+        System.out.println("[" + dateTime + "] [" + level.name() + "] [" + source + "] - " + message);
+    }
 }
